70e3b8a24a699687fe5b28304ed345b583b11ad2
sangitaaa12 <128121384+sangitaaa12@users.noreply.github.com>
2023-09-20 19:09:38 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./register";
-
-registerServiceWorker();
-
-ReactDOM.render(<App />, document.getElementById("root"));
